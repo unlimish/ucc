afa5e6a5eaa7014fd29788c5b497a6137e8eaa76
unlimish <mail@unlimish.xyz>
2020-10-10 16:54:43 +0900

    Make tokenize func without main func

--- ucc.c
+++ ucc.c
@@ -27,8 +27,94 @@ struct Token
 // Now pointing token
 Token *token;
 
-/* TODO HERE/
-/  Error sending function */
+// Error sending function
+// Get same arg printf
+void error(char *fmt, ...)
+{
+	va_list ap;
+	va_start(ap, fmt);
+	vfprintf(stderr, fmt, ap);
+	fprintf(stderr, "\n");
+	exit (1);
+}
+
+bool consume(char op)
+{
+	if (token->kind != TK_RESERVED || token->str[0] != op)
+		return false;
+	token = token->next;
+	return (true);
+}
+
+// If next token is expected symbol, read next one token
+// If it is not, make error
+void expect(char op)
+{
+	if (token->kind != TK_RESERVED || token->str[0] != op)
+		error("It is not '%c'", op);
+	token = token->next;
+}
+
+// If next toke is int, read next token then return it
+// If it is not, return error
+int expect_number()
+{
+	if (token->kind != TK_NUM)
+		error("It is not int");
+	int val = token->val;
+	token = token->next;
+	return (val);
+}
+
+bool at_eof()
+{
+	return (token->kind == TK_EOF);
+}
+
+// Make new token then conect to cur
+Token *new_token(TokenKind kind, Token *cur, char *str)
+{
+	Token *tok = calloc(1, sizeof(Token));
+	tok->kind = kind;
+	tok->str = str;
+	cur->next = tok;
+	return (tok);
+}
+
+// Tokenize input char p then return it
+Token *tokenize(char *p)
+{
+	Token head;
+	head.next = NULL;
+	Token *cur = &head;
+
+	while (*p)
+	{
+		if (isspace(*p))
+		{
+			p++;
+			continue;
+		}
+
+		if (*p == '+' || *p == '-')
+		{
+			cur = new_token(TK_RESERVED, cur, p++);
+			continue;
+		}
+
+		if (isdigit(*p))
+		{
+			cur = new_token(TK_NUM, cur, p);
+			cur->val = strtol(p, &p, 10);
+			continue;
+		}
+
+		error("Cannot tokenize");
+	}
+
+	new_token(TK_EOF, cur, p);
+	return (head.next);
+}
 
 int main(int argc, char *argv[])
 {
