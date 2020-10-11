85cfcad6b349689b1f124c588b81da412703218d
unlimish <mail@unlimish.xyz>
2020-10-11 22:12:50 +0900

    Add new node func

--- ucc.c
+++ ucc.c
@@ -39,6 +39,22 @@ struct Node
   int val;        // use it if kind is ND_NUM
 };
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return (node);
+}
+
+Node *new_node_num(int val)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return (node);
+}
 
 // Category token
 typedef enum {
