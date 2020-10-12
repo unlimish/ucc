6f7e666deb6c40dba000693a1bae37b30ae2cda3
unlimish <mail@unlimish.xyz>
2020-10-12 22:53:31 +0900

    Add multiple, div and ()

--- ucc.c
+++ ucc.c
@@ -19,120 +19,6 @@ void error_at(char *loc, char *fmt, ...) {
   exit(1);
 }
 
-
-// Node type of abstract syntax tree
-typedef enum
-{
-  ND_ADD, // +
-  ND_SUB, // -
-  ND_MUL, // *
-  ND_DIV, // /
-  ND_NUM, // int
-} NodeKind;
-
-typedef struct Node Node;
-
-struct Node
-{
-  NodeKind kind;  // Type of node
-  Node *lhs;      // Left side
-  Node *rhs;      // Right side
-  int val;        // use it if kind is ND_NUM
-};
-
-Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
-{
-  Node *node = calloc(1, sizeof(Node));
-  node->kind = kind;
-  node->lhs = lhs;
-  node->rhs = rhs;
-  return (node);
-}
-
-Node *new_node_num(int val)
-{
-  Node *node = calloc(1, sizeof(Node));
-  node->kind = ND_NUM;
-  node->val = val;
-  return (node);
-}
-
-Node *expr()
-{
-  Node *node = mul();
-
-  for (;;)
-  {
-    if (consume('+'))
-      node = new_node(ND_ADD, node, mul());
-    else if (consume('-'))
-      node = new_node(ND_SUB, node, mul());
-    else
-      return (node);
-  }
-}
-
-Node *mul()
-{
-  Node *node = primary();
-
-  for (;;)
-  {
-    if (consume('*'))
-      node = new_node(ND_MUL, node, primary());
-    else if (consume('/'))
-      node = new_node(ND_DIV, node, primary());
-    else
-      return (node);
-  }
-}
-
-Node *primary()
-{
-  if (consume('('))
-  {
-    Node *node = expr;
-    expect(')');
-    return (node);
-  }
-
-  return new_node_num(expect_number());
-}
-
-void gen(Node *node)
-{
-  if (node->kind == ND_NUM)
-  {
-    printf("  push %d\n", node->val);
-    return ;
-  }
-
-  gen(node->lhs);
-  gen(node->rhs);
-
-  printf("  pop rdi\n");
-  printf("  pop rax\n");
-
-  switch (node->kind)
-  {
-  case ND_ADD:
-    printf("  add rax, rdi\n");
-    break;
-  case ND_SUB:
-    printf("  sub rax, rdi\n");
-    break;
-  case ND_MUL:
-    printf("  imul rax, rdi\n");
-    break;
-  case ND_DIV:
-    printf("  cqo\n");
-    printf("  idiv rdi\n");
-    break;
-  }
-
-  printf("  push rax\n");
-}
-
 // Category token
 typedef enum {
   TK_RESERVED, // symbols
@@ -167,7 +53,7 @@ bool consume(char op) {
   if (token->kind != TK_RESERVED || token->str[0] != op)
     return false;
   token = token->next;
-  return (true);
+  return true;
 }
 
 // If next token is expected symbol, read next one token
@@ -229,35 +115,150 @@ Token *tokenize(char *p) {
   return (head.next);
 }
 
+// Node type of abstract syntax tree
+typedef enum
+{
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // int
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node
+{
+  NodeKind kind;  // Type of node
+  Node *lhs;      // Left side
+  Node *rhs;      // Right side
+  int val;        // use it if kind is ND_NUM
+};
+
+Node *new_node(NodeKind kind)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  return (node);
+}
+
+Node *new_binary(NodeKind kind, Node *lhs, Node *rhs)
+{
+  Node *node = new_node(kind);
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return (node);
+}
+
+Node *new_num(int val)
+{
+  Node *node = new_node(ND_NUM);
+  node->val = val;
+  return (node);
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+Node *expr()
+{
+  Node *node = mul();
+
+  for (;;)
+  {
+    if (consume('+'))
+      node = new_binary(ND_ADD, node, mul());
+    else if (consume('-'))
+      node = new_binary(ND_SUB, node, mul());
+    else
+      return (node);
+  }
+}
+
+Node *mul()
+{
+  Node *node = primary();
+
+  for (;;)
+  {
+    if (consume('*'))
+      node = new_binary(ND_MUL, node, primary());
+    else if (consume('/'))
+      node = new_binary(ND_DIV, node, primary());
+    else
+      return (node);
+  }
+}
+
+Node *primary()
+{
+  if (consume('('))
+  {
+    Node *node = expr();
+    expect(')');
+    return (node);
+  }
+
+  return new_num(expect_number());
+}
+
+void gen(Node *node)
+{
+  if (node->kind == ND_NUM)
+  {
+    printf("  push %d\n", node->val);
+    return ;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind)
+  {
+  case ND_ADD:
+    printf("  add rax, rdi\n");
+    break;
+  case ND_SUB:
+    printf("  sub rax, rdi\n");
+    break;
+  case ND_MUL:
+    printf("  imul rax, rdi\n");
+    break;
+  case ND_DIV:
+    printf("  cqo\n");
+    printf("  idiv rdi\n");
+    break;
+  }
+
+  printf("  push rax\n");
+}
+
 int main(int argc, char *argv[]) {
   if (argc != 2) {
     fprintf(stderr, "Argment amount is wrong\n");
     return (1);
   }
 
-  // Tokenize
-  token = tokenize(argv[1]);
+  // Tokenize then perse
+  user_input = argv[1];
+  token = tokenize(user_input);
+  Node *node = expr();
 
   // Output first part of assembly
-
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
 
-  // Check the input starts from int then output first mov
-  printf("  mov rax, %d\n", expect_number());
+  // Gen code with downing abstract tree
+  gen(node);
 
-  // input '+ <num>' or '- <num>' then output mov
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  // The result is on stack top
+  // Load to RAX then make it as return function
+  printf("  pop rax\n");
   printf("  ret\n");
   return (0);
 }
